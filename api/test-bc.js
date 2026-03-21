d82441dab608ed756e822b9e02a1ebda4e9153fc
vendasonline445-svg <vendasonline445@gmail.com>
2026-03-21 21:42:16 +0000

    debug: test multiple bc advertiser endpoints

--- api/test-bc.js
+++ api/test-bc.js
@@ -5,23 +5,29 @@ export default async function handler(req, res) {
     })
     var tokenData = await tokenRes.json()
     var token = tokenData.data.access_token
-
     var bcId = '7608040000504234001'
-    var url = 'https://business-api.tiktok.com/open_api/v1.3/bc/advertiser/get/?bc_id=' + bcId + '&page=1&page_size=10'
-    
-    var r = await fetch(url, {
-      headers: { 'Access-Token': token, 'Content-Type': 'application/json' }
-    })
-    
-    var text = await r.text()
-    
-    res.json({
-      status: r.status,
-      content_type: r.headers.get('content-type'),
-      body_preview: text.substring(0, 500),
-      body_length: text.length
-    })
+
+    var urls = [
+      'https://business-api.tiktok.com/open_api/v1.3/bc/advertiser/get/?bc_id=' + bcId + '&page=1&page_size=10',
+      'https://business-api.tiktok.com/open_api/v1.3/bc/advertiser/get?bc_id=' + bcId + '&page=1&page_size=10',
+      'https://business-api.tiktok.com/open_api/v1.3/bc/asset/get/?bc_id=' + bcId + '&asset_type=ADVERTISER&page=1&page_size=10',
+    ]
+
+    var results = []
+    for (var i = 0; i < urls.length; i++) {
+      var r = await fetch(urls[i], {
+        headers: { 'Access-Token': token, 'Content-Type': 'application/json' }
+      })
+      var text = await r.text()
+      results.push({
+        url: urls[i].replace(token, 'TOKEN').replace(bcId, 'BC_ID'),
+        status: r.status,
+        body: text.substring(0, 300)
+      })
+    }
+
+    res.json(results)
   } catch(err) {
-    res.status(500).json({ error: err.message, stack: err.stack })
+    res.status(500).json({ error: err.message })
   }
 }
